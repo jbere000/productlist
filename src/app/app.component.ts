c0c6e24e1099d2090e561c96fc7d0d7458705655
Jesus Bereguiain <jesus.bereguiain@invesyde.com>
2020-02-14 16:48:31 +0100

    navbar and routing fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { ProductListComponent } from './product-list/product-list.component';
 @Component({
   selector: 'app-root',
-  template:
-  `
-  <div>
-  <h1>{{title}}</h1>
-  <app-product-list></app-product-list>
-  </div>
-  `,
+  template: `
+  <nav class='navbar navbar-expanded navbar-light bg-light'>
+    <a class= 'navbar-brand'>{{title}}</a>
+  <ul class = 'nav nav-pills'>
+    <li><a class='nav-link' [routerLink]="['/welcome']">Home</a><li>
+    <li><a class='nav-link' [routerLink]="['/products']">Product List</a><li>
+  </ul>
+</nav>
+  <router-outlet></router-outlet>
+    `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,7 +5,6 @@ import { IProduct } from './product';
 import {ProductService} from './product.service';
 
 @Component({
-  selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
