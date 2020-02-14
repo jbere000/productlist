aacb9659b6ed82fd2279c90da68999bfb1c8f34a
Jesus Bereguiain <jesus.bereguiain@invesyde.com>
2020-02-14 16:29:02 +0100

    product detail, welcome, 404 components & Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,25 +10,38 @@ import { registerLocaleData } from '@angular/common';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star/star.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './product-list/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import {RouterModule} from '@angular/router';
+import { PagenotfoundComponent } from './home/pagenotfound.component';
 registerLocaleData(localeEs);
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', component: PagenotfoundComponent }
+    ])
   ],
   providers: [{
     provide: LOCALE_ID,
     useValue: 'es-ES'
   }
-
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/product-list/product-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from './product';
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product Detail';
+  product: IProduct;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
