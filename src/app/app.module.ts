50e8dcaed710b8051377789e0dc3c8fd370cb245
Jesus Bereguiain <jesus.bereguiain@invesyde.com>
2020-02-17 14:00:32 +0100

    separated modules app product shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import {LOCALE_ID} from '@angular/core';
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ProductDetailComponent } from './product-list/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import {RouterModule} from '@angular/router';
 import { PagenotfoundComponent } from './home/pagenotfound.component';
-import {ProductDetailGuard} from './product-list/product-detail.guard';
+import { ProductModule } from './product-list/product.module';
 registerLocaleData(localeEs);
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent,
     PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
+    ProductModule,
+    AppRoutingModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PagenotfoundComponent }
